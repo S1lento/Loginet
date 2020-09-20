13373aa0496ec15b2f7fb7f97d0a5e90d3753560
Eugene <e.borzunov@mediasoft.team>
2020-09-21 02:31:03 +0300

    delete comments

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,16 +6,7 @@ export const fetchData = (currentId) => async (dispatch) => {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/todos/${currentId}`
   );
-  // this case to any delay as u want, it can work without it
 
-  // setTimeout(() => {
-  //   dispatch({
-  //     type: GET_DATA,
-  //     payload: response.data,
-  //   });
-  // }, 7000);
-
-  // like below
   dispatch({
     type: GET_DATA,
     payload: response.data,
